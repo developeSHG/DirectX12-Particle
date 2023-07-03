530703e73a223364099531fc2ca7e9fd07d69f41
developeSHG <developeSHG@gmail.com>
2023-07-03 21:43:49 +0900

    [Compute Shader] Graphics용과 Compute용 따로 분리  - cmdlist, rootsignature

--- Game/Engine/RootSignature.h
+++ Game/Engine/RootSignature.h
@@ -5,14 +5,16 @@ class RootSignature
 public:
 	void Init();
 
-	ComPtr<ID3D12RootSignature>	GetSignature() { return _signature; }
+	ComPtr<ID3D12RootSignature>	GetGraphicsRootSignature() { return _graphicsRootSignature; }
+	ComPtr<ID3D12RootSignature>	GetComputeRootSignature() { return _computeRootSignature; }
 
 private:
-	void CreateSamplerDesc();
-	void CreateRootSignature();
+	void CreateGraphicsRootSignature();
+	void CreateComputeRootSignature();
 
 private:
-	ComPtr<ID3D12RootSignature>	_signature;
-	D3D12_STATIC_SAMPLER_DESC _samplerDesc;
+	D3D12_STATIC_SAMPLER_DESC	_samplerDesc; 
+	ComPtr<ID3D12RootSignature>	_graphicsRootSignature;	
+	ComPtr<ID3D12RootSignature>	_computeRootSignature;
 };
 
