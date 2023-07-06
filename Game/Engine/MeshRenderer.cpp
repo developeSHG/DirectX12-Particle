418c4ef50a3f27672da8973fddbbbb48f51cedd1
developeSHG <developeSHG@gmail.com>
2023-07-07 01:11:56 +0900

    [Instancing] 인스턴싱 버퍼를 관리하는 Manager 클래스 생성  - 같은 물체끼리는 한 번에 그려주기

--- Game/Engine/MeshRenderer.cpp
+++ Game/Engine/MeshRenderer.cpp
@@ -3,6 +3,7 @@
 #include "Mesh.h"
 #include "Material.h"
 #include "Transform.h"
+#include "InstancingBuffer.h"
 
 MeshRenderer::MeshRenderer() : Component(COMPONENT_TYPE::MESH_RENDERER)
 {
@@ -16,7 +17,24 @@ MeshRenderer::~MeshRenderer()
 
 void MeshRenderer::Render()
 {
-	 GetTransform()->PushData();
+	GetTransform()->PushData();
 	_material->PushGraphicsData();
 	_mesh->Render();
+}
+
+void MeshRenderer::Render(shared_ptr<InstancingBuffer>& buffer)
+{
+	buffer->PushData();
+	_material->PushGraphicsData();
+	_mesh->Render(buffer);
+}
+
+uint64 MeshRenderer::GetInstanceID()
+{
+	if (_mesh == nullptr || _material == nullptr)
+		return 0;
+
+	//uint64 id = (_mesh->GetID() << 32) | _material->GetID();
+	InstanceID instanceID{ _mesh->GetID(), _material->GetID() };
+	return instanceID.id;
 }
